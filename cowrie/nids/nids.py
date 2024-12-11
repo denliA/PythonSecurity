888671e2a29b20cbadae3799fb4b3005ad4c9ca0
Marwane AZZOUZI <marwane.azzouzi@epita.fr>
2024-12-11 12:24:07 -0500

    added nids container.

---
+++ cowrie/nids/nids.py
@@ -0,0 +1,10 @@
+from scapy.all import *
+from datetime import datetime
+
+if __name__ == "__main__":
+    print("NIDS service started... Monitoring network traffic.")
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print("NIDS service stopping...")
